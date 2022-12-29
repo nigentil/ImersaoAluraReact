85fca9ba5a18d198f573a2832170d99ebfe755ad
nival goncalves <nigentil@hotmail.com>
2022-12-29 19:14:22 -0300

    alteracoes playlists

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "02b588547bd526d6"; }
+/******/ 		__webpack_require__.h = function() { return "95d3145e7485df3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
